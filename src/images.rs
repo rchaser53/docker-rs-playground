47406379b0cdc6e8c5fedac842526c29870bad12
rchaser53 <tayoshizawa29@gmail.com>
2020-02-23 20:01:16 +0900

    try to create mock implement
    but it's failed

--- src/images.rs
+++ src/images.rs
@@ -5,7 +5,7 @@ use crate::builder::Builder;
 use crate::request::RequestBuilder;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct DockerImage {
     pub repo_tags: Vec<String>,
@@ -31,15 +31,6 @@ impl Image {
         }
     }
 
-    pub async fn get_images(&self) -> Result<Vec<DockerImage>> {
-        let bytes = self.builder.get("/images/json?digests=1").await?;
-
-        match serde_json::from_str(&bytes) {
-            Ok(data) => Ok(data),
-            Err(err) => bail!("{}", err),
-        }
-    }
-
     pub async fn pull_image(&self, image_name: &str, tag: &str) -> Result<Vec<DockerImagePull>> {
         let result = self
             .builder
@@ -62,19 +53,26 @@ impl Image {
     }
 }
 
+pub fn convert_docker_images(input: &str) -> Result<Vec<DockerImage>> {
+    match serde_json::from_str(&input) {
+        Ok(data) => Ok(data),
+        Err(err) => bail!("{}", err),
+    }
+}
+
 pub trait Req {}
 impl Req for Image {}
 
 #[async_trait]
 pub trait IsReq {
-    async fn get(&self) -> Result<String>;
+    async fn get(&self, target_url: &str) -> Result<String>;
 }
 
 #[async_trait]
 impl<T: Req + Sync> IsReq for T {
-    async fn get(&self) -> Result<String> {
+    async fn get(&self, target_url: &str) -> Result<String> {
         let builder: Builder = Default::default();
-        let result = builder.get("/images/json?digests=1").await?;
+        let result = builder.get(target_url).await?;
         Ok(result)
     }
 }
@@ -90,31 +88,75 @@ impl HaveReq for Image {
     }
 }
 
+#[async_trait]
+pub trait ImageTrait {
+    async fn get_images(&self) -> Result<Vec<DockerImage>>;
+}
+
+#[async_trait]
+impl ImageTrait for Image {
+    async fn get_images(&self) -> Result<Vec<DockerImage>> {
+        let bytes = self.get_req().get("/images/json").await?;
+        convert_docker_images(&bytes)
+    }
+}
+
 mod test {
-    use anyhow::{bail, Result};
+    use anyhow::Result;
     use async_trait::async_trait;
     use tokio::runtime::Runtime;
 
-    use super::{Builder, HaveReq, Image, Req};
+    use super::{convert_docker_images, DockerImage, Image, Req};
 
     #[test]
     fn test_req() {
         #[async_trait]
         pub trait IsReq {
-            async fn get(&self) -> Result<String>;
+            async fn get(&self, target_url: &str) -> Result<String>;
         }
 
         #[async_trait]
         impl<T: Req + Sync> IsReq for T {
-            async fn get(&self) -> Result<String> {
-                Ok("test".to_string())
+            async fn get(&self, target_url: &str) -> Result<String> {
+                Ok(r#"[{ "RepoTags": [], "Id": "test" }]"#.to_string())
             }
         }
+
+        pub trait HaveReq {
+            type A: IsReq;
+            fn get_req(&self) -> &Self::A;
+        }
+        impl HaveReq for Image {
+            type A = Self;
+            fn get_req(&self) -> &Self::A {
+                &self
+            }
+        }
+
+        #[async_trait]
+        pub trait ImageTrait {
+            async fn get_images(&self) -> Result<Vec<DockerImage>>;
+        }
+
+        #[async_trait]
+        impl ImageTrait for Image {
+            async fn get_images(&self) -> Result<Vec<DockerImage>> {
+                let bytes = self.get_req().get("/images/json").await?;
+                convert_docker_images(&bytes)
+            }
+        }
+
         let mut rt = Runtime::new().unwrap();
 
         let image = Image::new();
-        let result = rt.block_on(image.get_req().get());
+        let result = rt.block_on(image.get_images());
 
-        assert_eq!(result.unwrap(), "test");
+        assert_eq!(
+            result.unwrap(),
+            vec![DockerImage {
+                repo_tags: vec![],
+                id: "test".to_string()
+            }]
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ mod builder;
 use builder::Builder;
 
 mod images;
+use images::ImageTrait;
 mod request;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let client: Builder = Default::default();
     let image = client.image();
-    let images = image.pull_image("busyb_ox", "latest").await;
+    // let images = image.pull_image("busyb_ox", "latest").await;
+    let images = image.get_images().await;
 
     println!("{:?}", images);
     Ok(())
