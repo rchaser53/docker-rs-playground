8040d7269845eac81a373c7311e4812ab96894d6
rchaser53 <tayoshizawa29@gmail.com>
2020-02-23 21:10:33 +0900

    test deserialize function only

--- src/images.rs
+++ src/images.rs
@@ -1,18 +1,17 @@
 use anyhow::{bail, Result};
-use async_trait::async_trait;
 
 use crate::builder::Builder;
 use crate::request::RequestBuilder;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct DockerImage {
     pub repo_tags: Vec<String>,
     pub id: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 #[serde(untagged)]
 pub enum DockerImagePull {
     Success { status: String },
@@ -31,132 +30,79 @@ impl Image {
         }
     }
 
+    pub async fn get_images(&self) -> Result<Vec<DockerImage>> {
+        let bytes = self.builder.get("/images/json").await?;
+        deserialize_docker_images(&bytes)
+    }
+
     pub async fn pull_image(&self, image_name: &str, tag: &str) -> Result<Vec<DockerImagePull>> {
-        let result = self
+        let bytes = self
             .builder
             .post(
                 &format!("/images/create?fromImage={}&tag={}", image_name, tag),
                 vec![],
             )
-            .await?
-            .split("\r\n")
-            .filter(|s| *s != "")
-            .map(|s| {
-                let result: DockerImagePull = match serde_json::from_str(&s) {
-                    Ok(data) => data,
-                    Err(err) => panic!("{}", err),
-                };
-                result
-            })
-            .collect::<Vec<DockerImagePull>>();
-        Ok(result)
+            .await?;
+        deserialize_docker_pull_images(&bytes)
     }
 }
 
-pub fn convert_docker_images(input: &str) -> Result<Vec<DockerImage>> {
+pub fn deserialize_docker_images(input: &str) -> Result<Vec<DockerImage>> {
     match serde_json::from_str(&input) {
         Ok(data) => Ok(data),
         Err(err) => bail!("{}", err),
     }
 }
 
-pub trait Req {}
-impl Req for Image {}
-
-#[async_trait]
-pub trait IsReq {
-    async fn get(&self, target_url: &str) -> Result<String>;
-}
-
-#[async_trait]
-impl<T: Req + Sync> IsReq for T {
-    async fn get(&self, target_url: &str) -> Result<String> {
-        let builder: Builder = Default::default();
-        let result = builder.get(target_url).await?;
-        Ok(result)
-    }
-}
-
-pub trait HaveReq {
-    type A: IsReq;
-    fn get_req(&self) -> &Self::A;
-}
-impl HaveReq for Image {
-    type A = Self;
-    fn get_req(&self) -> &Self::A {
-        &self
-    }
-}
-
-#[async_trait]
-pub trait ImageTrait {
-    async fn get_images(&self) -> Result<Vec<DockerImage>>;
-}
-
-#[async_trait]
-impl ImageTrait for Image {
-    async fn get_images(&self) -> Result<Vec<DockerImage>> {
-        let bytes = self.get_req().get("/images/json").await?;
-        convert_docker_images(&bytes)
-    }
+pub fn deserialize_docker_pull_images(input: &str) -> Result<Vec<DockerImagePull>> {
+    let result = input
+        .split("\n")
+        .filter(|s| *s != "")
+        .map(|s| {
+            dbg!(&s);
+            let result: DockerImagePull = match serde_json::from_str(&s) {
+                Ok(data) => data,
+                Err(err) => panic!("{}", err),
+            };
+            result
+        })
+        .collect::<Vec<DockerImagePull>>();
+    Ok(result)
 }
 
 mod test {
-    use anyhow::Result;
-    use async_trait::async_trait;
-    use tokio::runtime::Runtime;
-
-    use super::{convert_docker_images, DockerImage, Image, Req};
+    use super::*;
 
     #[test]
-    fn test_req() {
-        #[async_trait]
-        pub trait IsReq {
-            async fn get(&self, target_url: &str) -> Result<String>;
-        }
-
-        #[async_trait]
-        impl<T: Req + Sync> IsReq for T {
-            async fn get(&self, target_url: &str) -> Result<String> {
-                Ok(r#"[{ "RepoTags": [], "Id": "test" }]"#.to_string())
-            }
-        }
-
-        pub trait HaveReq {
-            type A: IsReq;
-            fn get_req(&self) -> &Self::A;
-        }
-        impl HaveReq for Image {
-            type A = Self;
-            fn get_req(&self) -> &Self::A {
-                &self
-            }
-        }
-
-        #[async_trait]
-        pub trait ImageTrait {
-            async fn get_images(&self) -> Result<Vec<DockerImage>>;
-        }
-
-        #[async_trait]
-        impl ImageTrait for Image {
-            async fn get_images(&self) -> Result<Vec<DockerImage>> {
-                let bytes = self.get_req().get("/images/json").await?;
-                convert_docker_images(&bytes)
-            }
-        }
-
-        let mut rt = Runtime::new().unwrap();
-
-        let image = Image::new();
-        let result = rt.block_on(image.get_images());
+    fn deseriazlie_docker_images_success() {
+        let converted = deserialize_docker_images(r#"[{ "RepoTags": [], "Id": "test" }]"#);
 
         assert_eq!(
-            result.unwrap(),
+            converted.unwrap(),
             vec![DockerImage {
                 repo_tags: vec![],
                 id: "test".to_string()
             }]
+        );
+    }
+
+    #[test]
+    fn deserialize_docker_pull_images_success() {
+        let converted = deserialize_docker_pull_images(
+            r#"{ "message": "test" }
+{ "status": "succeeded" }"#,
+        );
+
+        assert_eq!(
+            converted.unwrap(),
+            vec![
+                DockerImagePull::Failed {
+                    message: "test".to_string()
+                },
+                DockerImagePull::Success {
+                    status: "succeeded".to_string()
+                }
+            ]
         );
     }
 }
