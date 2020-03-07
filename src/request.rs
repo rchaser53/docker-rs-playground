1c67800948c001d68085735935dee3286550b80f
rchaser53 <tayoshizawa29@gmail.com>
2020-03-07 23:16:07 +0900

    add XRegistryAuth

--- src/request.rs
+++ src/request.rs
@@ -31,14 +31,15 @@ impl RequestBuilder for Builder {
         Ok(String::from_utf8(bytes).unwrap())
     }
 
-    async fn post<S: Into<Body> + Send>(&self, target_url: &str, body: S) -> Result<String> {
+    async fn post<S>(&self, target_url: &str, body: S) -> Result<String>
+    where
+        S: Into<Body> + Send,
+    {
         let url: Uri = Uri::new(&self.base_url, target_url).into();
         let request = Request::builder()
             .method("POST")
             .uri(url)
-            .body(body.into())
-            // .header("X-Registry-Auth", )
-            .unwrap();
+            .body(body.into())?;
         let response_body = self.client.request(request).await?.into_body();
         let bytes = response_body
             .try_fold(Vec::default(), |mut v, bytes| async {
@@ -54,7 +55,9 @@ impl RequestBuilder for Builder {
 #[async_trait]
 pub trait RequestBuilder {
     async fn get(&self, target_url: &str) -> Result<String>;
-    async fn post<S: Into<Body> + Send>(&self, target_url: &str, body: S) -> Result<String>;
+    async fn post<S>(&self, target_url: &str, body: S) -> Result<String>
+    where
+        S: Into<Body> + Send;
 }
 
 pub fn serialize_base64<T>(input: T) -> Result<String>
@@ -72,13 +75,23 @@ pub struct XRegistryAuth {
     pub email: String,
 }
 
+impl XRegistryAuth {
+    pub fn new(username: String, password: String, email: String) -> Self {
+        XRegistryAuth {
+            username,
+            password,
+            email,
+        }
+    }
+}
+
 #[test]
 fn serialize_base64_test() {
-    let input = XRegistryAuth {
-        username: String::from("test_name"),
-        password: String::from("test_password"),
-        email: String::from("test@gmail.com"),
-    };
+    let input = XRegistryAuth::new(
+        String::from("test_name"),
+        String::from("test_password"),
+        String::from("test@gmail.com"),
+    );
     let expected = encode(
         "{\"username\":\"test_name\",\"password\":\"test_password\",\"email\":\"test@gmail.com\"}",
     );
